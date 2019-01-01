214ccfdddb1ab0e580948f8c2e0c1d07a091384b
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2019-01-01 19:05:32 +0530

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -837,8 +837,9 @@ async def buy():
            await client.say('Thank you for using this command ! Please keep in mind that you can cancel this process at any time with /cancel .You can check our current prices in the #rate-info channel! What do you want to buy? Or Instant points or deluxe?  Just write it to me smiley .')
 @client.command(pass_context=True)
 async def instant():
-           await client.say('By buying this points you can generate all trivia lives without a phone number in just one second! Pricing for instant points:1 point = 4 loco life / 1 point = 4 brain bazzi life / 1 point = 1 hq Life / 1 point = 4 SWOO life  .  @ You can send your lives to as many different HQ Trivia accounts as you want, as long as you have enough points left. Under 100 lives: 12 rs each Equal or over 100 lives: 10 rs each ! How many points do you want?  Please type /want <number> points.')
+           await client.say('By buying this points you can generate all trivia lives without a phone number in just one second! Pricing for instant points:1 point = 4 loco life / 1 point = 4 brain bazzi life / 1 point = 1 hq Life / 1 point = 4 SWOO life  .  @ You can send your lives to as many different HQ Trivia accounts as you want, as long as you have enough points left. Under 100 lives: 12 rs each Equal or over 100 lives: 10 rs each ! How many points do you want?  Please type /want.')
 @client.command(pass_context=True)
-async def want <number> points():
+async def want():
            await client.say('Ok relaxed️ , your rate will keep multiple by 12rs /point if less then 100 / if more then then 10rs /point . Is that okay for you?? If okay then  Please type /ok.')
+	
 	client.run(os.getenv('Token'))
