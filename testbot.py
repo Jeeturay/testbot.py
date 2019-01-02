87616e4eea282144a592c937308821a3bf91b38c
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2019-01-02 14:20:57 +0530

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -847,7 +847,7 @@ async def on_member_remove(member):
 
 @client.command(pass_context=True)
 async def buy():
-           await client.say(' Hellos bro Thanks for using buy command , BUT YOU CAN ONLY BUY POINTS IN OUR OFFICIAL SERVER if u are already in your official lives store server then proceed your buying process by typing /buy points (type /help for more information).')
+           await client.say(' Hello bro Thanks for using buy command , **__BUT YOU CAN ONLY BUY POINTS IN OUR OFFICIAL SERVER__** **if u are already in your official lives store server then proceed your buying process by typing /buy points** (*type /help for more information*).')
 @client.command(pass_context=True)
 async def buypoints():
            await client.say(' you have ```0``` point left..!!.')
