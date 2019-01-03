76a8a155c4506ebc4d97b71307e891cbab427294
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2019-01-03 11:56:39 +0530

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -864,9 +864,7 @@ async def stock(ctx):
     embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
     embed.set_author(name='stock')
     embed.add_field(name = '**__stock of SR.LIVES STORE for lives__** ',value ='**__LOCO__** ```198389``` .',inline = False)
-    await client.send_message(author,embed=embed)
-    await client.say('📨 Check DMs For Information')
-
+    
 
 client.run(os.getenv('Token')) 	       	 	       	       
 		
