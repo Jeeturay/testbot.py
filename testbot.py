9aad97efd2e06b61b4abbd6c6d9d04a83b4d27ac
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2019-01-03 11:12:23 +0530

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -839,5 +839,21 @@ async def on_member_remove(member):
   msg = "Goodbye {}".format(member.mention)
   await client.send_message(channel, msg) 
 
+@client.command(pass_context=True)
+async def buy():
+  await client.say(' Thanks for using buy command , **__BUT YOU CAN ONLY BUY POINTS IN OUR OFFICIAL SERVER__** **if u are already in your official lives store server then proceed your buying process by typing `/buypoints`** (*type `/srhelp` for more information*).')
+@client.command(pass_context=True)
+async def buypoints():
+  await client.say(' **Thank you for using this command !** Please keep in mind that you can cancel this process at any time with ***`/cancel`*** . You can check our current prices in the #rate-info channel !  **__What do you want to buy__** **Instant points or deluxe?** If instant then type ***`/instant`*** if :gem: then type ***`/deluxe`*** . Just write it to me :smiley: .')
+@client.command(pass_context=True)
+async def instant():
+  await client.say(' **With these points you can generate all trivia lives without a phone number in just one second ! Pricing for instant points:  `1 point = 4 loco life / 1 point = 4 brain bazzi life / 1 point = 1 hq Life / 1 point = 4 SWOO life` . Under 100 lives: 12 rs each . Equal or over 100 lives: 10 rs each How many points do you want? Please type `/want <amount>`** .')
+@client.command(pass_context=True)
+async def deluxe():
+  await client.say(' **You get daily points with the DELUXEgem option! You can use points to generate HQ lives without a phone number! Current price: amount per day in $ per week** *(For example:__**) 1 life per day --> 7 lives per week --> 63rs / $0.9/week ! 5 lives per day --> 35 lives per week --> 315rs / 4.5$/week ! 10 lives per day --> 70 lives per week --> 630rs /  9$/week* **__How many points do you want per day?__** **Please write it as** `!get <number>` .') .')
+@client.command(pass_context=True)
+async def points():
+  await client.say(' you have ```0```point left..!! .')
+
 client.run(os.getenv('Token')) 	       	 	       	       
 		
