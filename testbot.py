39b1dbf32d547ec98aad3df33e54ede2df031a8c
Jeeturay <45664400+Jeeturay@users.noreply.github.com>
2019-01-05 11:40:07 +0530

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -532,7 +532,7 @@ async def clear(ctx, number):
 async def ban(ctx,user:discord.Member):
 
     if user.server_permissions.ban_members:
-        await client.say('**He is mod/admin and i am unable to ban him/her**')
+        await client.say('**🚷Uhhhhh, you can't kick a moderator. ❎That user is a mod/admin, I can't do that**. **__And Your role is not high enough to kick this member__**.🚫😉')
         return
 
     try:
